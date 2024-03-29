2013a85cbf42d6282cec39069fae71c007cf5dfa
Vidhyashree4112001 <vidhyashree401@gmail.com>
2024-03-29 20:37:41 +0530

    Updated

---
+++ src/Image.jsx
@@ -0,0 +1,10 @@
+import image from "./task.jpg"
+function Image(){
+    return(
+   <div>
+    <img src={image} alt="image" />
+    <h2>Jullie Rabit ears</h2>
+   </div>
+    )
+}
+export default Image
